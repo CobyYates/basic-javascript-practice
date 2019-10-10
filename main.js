9a227a6a7b64e31dcd1792d9a9899393dafd9327
Coby Yates <cobylyates@gmail.com>
2019-10-10 17:30:27 -0600

    Added all api's, simplified JS not working yet

--- main.js
+++ main.js
@@ -43,6 +43,7 @@ films.forEach(function(film) {
     mainArea.appendChild(personDiv)
   });
 
+  //People images
 function getCharNumber(charURL) {
   let end = charURL.lastIndexOf('/')
   let charID = charURL.substring(end -2, end)
@@ -52,3 +53,128 @@ function getCharNumber(charURL) {
       return charID
   }
 }
+
+planets.forEach((planet) => {
+  let planetDiv = document.createElement('div')
+  let name = document.createElement('h1')
+  let diameter = document.createElement('p')
+  let terrain = document.createElement('p')
+  let population = document.createElement('p')
+  // let pic = document.createElement('img')
+
+  // let charNum = getCharNumber(planet.url)
+  // pic.setAttribute('class', 'photo')
+
+  name.textContent = planet.name
+  diameter.textContent = 'Diameter: ' + planet.diameter
+  terrain.textContent = 'Terrain: ' + planet.terrain
+  population.textContent = 'Population: ' + planet.population
+
+  planetDiv.appendChild(name)
+  // planetDiv.appendChild(pic)
+  planetDiv.appendChild(diameter)
+  planetDiv.appendChild(terrain)
+  planetDiv.appendChild(population)
+  
+
+  mainArea.appendChild(planetDiv)
+});
+
+species.forEach((specy) => {
+  let specyDiv = document.createElement('div')
+  let name = document.createElement('h1')
+  let classification = document.createElement('p')
+  let language = document.createElement('p')
+  // let pic = document.createElement('img')
+
+  // let charNum = getCharNumber(planet.url)
+  // pic.setAttribute('class', 'photo')
+
+  name.textContent = specy.name
+  classification.textContent = 'Classification: ' + specy.classification
+  language.textContent = 'Language: ' + specy.language
+
+  specyDiv.appendChild(name)
+  // planetDiv.appendChild(pic)
+  specyDiv.appendChild(classification)
+  specyDiv.appendChild(language)
+  
+
+  mainArea.appendChild(specyDiv)
+});
+
+starships.forEach((starship) => {
+  let starshipDiv = document.createElement('div')
+  let name = document.createElement('h1')
+  let model = document.createElement('p')
+  let cost = document.createElement('p')
+  let hyperdrive = document.createElement('p')
+  // let pic = document.createElement('img')
+
+  // let charNum = getCharNumber(planet.url)
+  // pic.setAttribute('class', 'photo')
+
+  name.textContent = starship.name
+  model.textContent = 'Model: ' + starship.model
+  cost.textContent = 'Cost: ' + starship.cost_in_credits + ' credits'
+  hyperdrive.textContent = 'Hyperdrive Rating: ' + starship.hyperdrive_rating
+
+  starshipDiv.appendChild(name)
+  // planetDiv.appendChild(pic)
+  starshipDiv.appendChild(model)
+  starshipDiv.appendChild(cost)
+  starshipDiv.appendChild(hyperdrive)
+  
+
+  mainArea.appendChild(starshipDiv)
+});
+
+vehicles.forEach((starship) => {
+  let starshipDiv = document.createElement('div')
+  let name = document.createElement('h1')
+  let model = document.createElement('p')
+  let cost = document.createElement('p')
+  let speedMax = document.createElement('p')
+  // let pic = document.createElement('img')
+
+  // let charNum = getCharNumber(planet.url)
+  // pic.setAttribute('class', 'photo')
+
+  name.textContent = starship.name
+  model.textContent = 'Model: ' + starship.model
+  cost.textContent = 'Cost: ' + starship.cost_in_credits + ' credits'
+  speedMax.textContent = 'Max Speed: ' + starship.max_atmosphering_speed
+
+  starshipDiv.appendChild(name)
+  // planetDiv.appendChild(pic)
+  starshipDiv.appendChild(model)
+  starshipDiv.appendChild(cost)
+  starshipDiv.appendChild(speedMax)
+  
+
+  mainArea.appendChild(starshipDiv)
+});
+
+api(films, title, opening_crawl)
+
+api((apiName, prop1, prop2, prop3, prop4, prop5) => {
+  apiName.forEach(() => {
+    let prop1Div = document.createElement('div')
+    let prop2 = document.createElement('h1')
+    let prop3 = document.createElement('p')
+    let prop4 = document.createElement('p')
+    let prop5 = document.createElement('p')
+  
+    prop2.textContent = prop1.prop2
+    prop3.textContent = 'prop3: ' + prop1.prop3
+    prop4.textContent = 'prop4: ' + prop1.prop4
+    prop5.textContent = 'prop5: ' + prop1.prop5
+  
+    prop1Div.appendChild(prop2)
+    prop1Div.appendChild(prop3)
+    prop1Div.appendChild(prop4)
+    prop1Div.appendChild(prop5)
+  
+    mainArea.appendChild(prop1Div)
+  });
+})
